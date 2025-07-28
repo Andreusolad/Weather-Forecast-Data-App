a85599c34986a588545f122a5476ae8e1a4c184b
andreusola <140717026+andreusola@users.noreply.github.com>
2025-07-28 15:02:55 +0200

    Day 33: Complete the App, now it's finished

--- main.py
+++ main.py
@@ -9,24 +9,31 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+st.subheader(f"{option} for the next {days} days in {place} (forecast every 3 hours)")
 
-if place:
-    # Get the temperature and sky data
-    filtered_data = get_data(place, days)
+try:
+    if place:
+        # Get the temperature and sky data
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y":"Temperature (ºC)"})
-        # Method responsible to create a graph
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [(dict["main"]["temp"] / 10) for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y":"Temperature (ºC)"})
+            # Method responsible to create a graph
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        image_paths = [images[condition] for condition in sky_conditions]
-        st.image(image_paths, width=115)
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+except KeyError:
+    st.markdown(
+        "<h4 style='color: red;'>Entered place not found in the dataset :(</h4>",
+        unsafe_allow_html=True
+    )
+
 
